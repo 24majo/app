412095cf1991ee9e1c61c592c762e547c8e15435
Alondra Guerrero Arenas <marijoguerrero24@gmail.com>
2025-03-24 16:41:22 -0600

    feat: Initialize project with React and Mantine, adding basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import cx from 'clsx';
+import { MantineProvider, Container, createTheme, Button } from '@mantine/core';
+import classes from './styles/Demo.module.css';
+import { TextInput, PasswordInput } from '@mantine/core';
+import { IconAt } from '@tabler/icons-react';
+import { IconEyeCheck, IconEyeOff } from '@tabler/icons-react';
 import './App.css';
 
 function App() {
+  const icon = <IconAt size={16} />
+  const theme = createTheme({
+    components: {
+      Container: Container.extend({
+        classNames: (_, { size }) => ({
+          root: cx({ [classes.responsiveContainer]: size === 'responsive' }),
+        }),
+      }),
+    },
+  });
+
+  const VisibilityToggleIcon = ({ reveal }: { reveal: boolean }) =>
+    reveal ? (
+      <IconEyeOff style={{ width: 'var(--psi-icon-size)', height: 'var(--psi-icon-size)' }} />
+    ) : (
+      <IconEyeCheck style={{ width: 'var(--psi-icon-size)', height: 'var(--psi-icon-size)' }} />
+    );
+
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  
+  const Cancelar = () => {
+    setEmail('');
+    setPass('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={theme}>
+      <Container size="responsive" bg="var(--mantine-color-blue-light)">
+        <TextInput
+          leftSectionPointerEvents="none"
+          leftSection={icon}
+          label ="Correo"
+          placeholder="nombre@dominio.com"
+          value={email}
+          error="Correo inválido"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <PasswordInput
+          maw={320}
+          mx="auto"
+          label="Contraseña"
+          placeholder="Contraseña"
+          defaultValue=""
+          visibilityToggleIcon={VisibilityToggleIcon}
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        />
+
+        <Button 
+          variant="filled" 
+          size="md" 
+          radius="md">Ingresar</Button>
+        <Button 
+          variant="filled" 
+          color="gray" 
+          size="md" 
+          radius="md"
+          onClick={Cancelar}>Cancelar</Button>
+      </Container>
+    </MantineProvider>
   );
 }
 
