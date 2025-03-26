67b6557d01b365bc1daa28c650be1c7289f13fc9
Alondra Guerrero Arenas <marijoguerrero24@gmail.com>
2025-03-26 17:26:55 -0600

    feat: add footer to navbar for user session

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -1,27 +1,43 @@
-import { IconChevronRight } from '@tabler/icons-react';
-import { Avatar, Group, Text, UnstyledButton } from '@mantine/core';
+import { IconChevronCompactUp, IconLogout } from '@tabler/icons-react';
+import { Avatar, Group, Text, UnstyledButton, Menu, Badge } from '@mantine/core';
 import classes from '../styles/user.module.css';
 
 export function UserButton() {
+
+  const names = [
+    'John Doe'
+  ];
+
+  const avatars = names.map((name) => <Avatar key={name} name={name} color="initials" />);
   return (
     <UnstyledButton className={classes.user}>
       <Group>
-        <Avatar
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
-          radius="xl"
-        />
+        {avatars}
 
         <div style={{ flex: 1 }}>
           <Text size="sm" fw={500}>
-            Harriette Spoonlicker
+            'John Doe'
           </Text>
 
-          <Text c="dimmed" size="xs">
-            Rol
-          </Text>
+          <Badge color="gray" radius="lg">Rol</Badge>
         </div>
 
-        <IconChevronRight size={20} stroke={1.5} />
+        <Menu shadow="md" width={200}>
+          <Menu.Target>
+            <IconChevronCompactUp size={20}/>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <Menu.Item
+              color="red"
+              leftSection={<IconLogout size={14} />}
+            >
+              Cerrar Sesión
+          </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+
+        
       </Group>
     </UnstyledButton>
   );
